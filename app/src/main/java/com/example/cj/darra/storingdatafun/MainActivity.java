80b1b0d831cebf6e835cc46c163dccd2501bbe55
KarlDarragjati <karl.darragjati@gmail.com>
2018-05-16 16:23:03 +0200

    03. Using Alert Dialogs

--- app/src/main/java/com/example/cj/darra/storingdatafun/MainActivity.java
+++ app/src/main/java/com/example/cj/darra/storingdatafun/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.cj.darra.storingdatafun;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +42,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are you sure!?")
+                .setMessage("Do you definitely want to do this?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 }
