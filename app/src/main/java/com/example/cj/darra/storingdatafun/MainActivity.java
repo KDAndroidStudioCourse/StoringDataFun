ce579c0d6a2de4b3d810b7189818183d8a0b3f32
KarlDarragjati <karl.darragjati@gmail.com>
2018-05-17 16:17:51 +0200

    05. SQLite Databases

--- app/src/main/java/com/example/cj/darra/storingdatafun/MainActivity.java
+++ app/src/main/java/com/example/cj/darra/storingdatafun/MainActivity.java
@@ -1,35 +1,17 @@
 package com.example.cj.darra.storingdatafun;
 
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.AlertDialog;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
-    ListView listView;
-    SharedPreferences sharedPreferences;
-
-    static ArrayList<String> notes = new ArrayList<>();
-    static ArrayAdapter arrayAdapter;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -37,19 +19,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         super.onOptionsItemSelected(item);
 
-        switch (item.getItemId()) {
-            case R.id.addNote:
-                Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
-                startActivity(intent);
-                return true;
-
-            case R.id.settings:
-                Log.i("Item Selected", "" + item.getTitle());
-                return true;
-
-            default:
-                return false;
-        }
+        return true;
     }
 
     @Override
@@ -57,51 +27,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.listView);
+        try {
+            SQLiteDatabase myDatabase = this.openOrCreateDatabase("Events", MODE_PRIVATE, null);
 
-        sharedPreferences = getApplicationContext().getSharedPreferences("com.example.cj.darra.storingdatafun", Context.MODE_PRIVATE);
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS events (name VARCHAR, year INT(4))");
 
-        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes", null);
+            myDatabase.execSQL("INSERT INTO events (name, year) VALUES ('Milenieum', 2000)");
+            myDatabase.execSQL("INSERT INTO events (name, year) VALUES ('Nick started teaching', 2014)");
 
-        if (set == null) {
-            notes.add("Example note.");
-        } else {
-            notes = new ArrayList<>(set);
+            Cursor c = myDatabase.rawQuery("SELECT * FROM events", null);
+
+            int nameIndex = c.getColumnIndex("name");
+            int yearIndex = c.getColumnIndex("year");
+
+            c.moveToFirst();
+
+            while (c != null) {
+                Log.i("name", "" + c.getString(nameIndex));
+                Log.i("year", "" + Integer.toString(c.getInt(yearIndex)));
+
+                c.moveToNext();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, notes);
-        listView.setAdapter(arrayAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
-                intent.putExtra("noteId", position);
-                startActivity(intent);
-            }
-        });
-
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
-                new AlertDialog.Builder(MainActivity.this)
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .setTitle("Are you sure?")
-                        .setMessage("Do you want to delete this note?")
-                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                notes.remove(position);
-                                arrayAdapter.notifyDataSetChanged();
-
-                                HashSet<String> set = new HashSet<>(MainActivity.notes);
-                                sharedPreferences.edit().putStringSet("notes", set).apply();
-                            }
-                        })
-                        .setNegativeButton("No", null)
-                        .show();
-                return true;
-            }
-        });
     }
 }
